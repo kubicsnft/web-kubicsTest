afe313559cc27991e5e1051e704510122c71fadf
SeviGit <pablo.sevi.ps@gmail.com>
2022-09-19 20:24:31 +0200

    add.imporDinamicCarousel

--- components/Newsletter.jsx
+++ components/Newsletter.jsx
@@ -7,7 +7,7 @@ export default function Newsletters(props) {
     // Button subcribe styles
     const style = {
         color: props.color,
-        style: ' border-2 border-primary butt '
+        style: '  butt '
     }
     // ---------------------
 
@@ -61,10 +61,8 @@ export default function Newsletters(props) {
                     type="button"
                     disabled={state === "LOADING"}
                     onClick={subscribe}>
-                    <p>
-                        <FormattedMessage
-                            id='app.butto'
-                            defaultMessage='Subscribe'/>
+                    <p className="">
+                        Subscribe
                     </p>
                     <svg strokeWidth="4" stroke="currentColor" viewBox="0 0 24 24" fill="none" className="w-6 h-6" xmlns="http://www.w3.org/2000/svg">
                         <path d="M14 5l7 7m0 0l-7 7m7-7H3" strokeLinejoin="round" strokeLinecap="round"></path>
