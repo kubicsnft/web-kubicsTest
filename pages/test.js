86c1bd36b60c9a89d3b3cb246455ed2301b41e2f
Miguel <migueportillaferrero@gmail.com>
2022-10-19 16:53:26 +0200

    trying to fix error with getServerSideProps v1

--- pages/test.js
+++ pages/test.js
@@ -1,25 +1,22 @@
-import NFTZone from "../components/mint_page/NFT_zone";
+import React from "react";
+import useSWR from "swr";
 
-export default function About({ data }) {
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
+export default function Test() {
+  const { data, error } = useSWR(
+    "http://localhost:3000/api/NFTsMongo/",
+    fetcher
+  );
+
+  if (error) return "An error has occurred.";
+  if (!data) return "Loading...";
   return (
     <div>
-      <NFTZone />
       <h1>{data.data[0].id}</h1>
       <h1>{data.data[1].id}</h1>
       <h1>{data.data[2].id}</h1>
       <h1>{data.data[3].id}</h1>
     </div>
   );
-}
-
-export async function getServerSideProps() {
-  // Fetch data from external API
-  const url = "http://localhost:3000/api/NFTsMongo/";
-
-  const res = await fetch(url);
-  const data = await res.json();
-
-  return {
-    props: { data }, // will be passed to the page component as props
-  };
 }
